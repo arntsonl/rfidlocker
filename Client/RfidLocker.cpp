b91fc7f911bcc5d6693c6a3204041e94f1efba12
Luke Arntson <arntsonl@gmail.com>
2015-10-20 00:21:05 -0400

    First commit, no functionality yet.

---
+++ Client/RfidLocker.cpp
@@ -0,0 +1,290 @@
+// RfidLocker.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "RfidLocker.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+NOTIFYICONDATA niData;
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+ULONGLONG			GetDllVersion(LPCTSTR lpszDllName);
+void				ShowContextMenu(HWND, DWORD, DWORD);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_RFIDLOCKER, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_RFIDLOCKER));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_RFIDLOCKER));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_RFIDLOCKER);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+VOID AddTrayIcon(HWND hWnd)
+{
+
+	// zero the structure - note: Some Windows funtions
+	// require this but I can't be bothered to remember
+	// which ones do and which ones don't.
+	ZeroMemory(&niData, sizeof(NOTIFYICONDATA));
+
+	// get Shell32 version number and set the size of the
+	// structure note: the MSDN documentation about this is
+	// a little dubious(see bolow) and I'm not at all sure
+	// if the code bellow is correct
+	ULONGLONG ullVersion = GetDllVersion(_T("Shell32.dll"));
+
+	if (ullVersion >= MAKEDLLVERULL(6, 0, 0, 0))
+		niData.cbSize = sizeof(NOTIFYICONDATA);
+	else if (ullVersion >= MAKEDLLVERULL(5, 0, 0, 0))
+		niData.cbSize = NOTIFYICONDATA_V2_SIZE;
+	else niData.cbSize = NOTIFYICONDATA_V1_SIZE;
+
+
+	// the ID number can be any UINT you choose and will
+	// be used to identify your icon in later calls to
+	// Shell_NotifyIcon
+	niData.uID = IDC_MYICON;
+
+	// state which structure members are valid
+	// here you can also choose the style of tooltip
+	// window if any - specifying a balloon window:
+	// NIF_INFO is a little more complicated 
+	niData.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+
+	// load the icon note: you should destroy the icon
+	// after the call to Shell_NotifyIcon
+	niData.hIcon =
+		(HICON)LoadImage(hInst,
+			MAKEINTRESOURCE(IDI_RFIDLOCKER),
+			IMAGE_ICON,
+			GetSystemMetrics(SM_CXSMICON),
+			GetSystemMetrics(SM_CYSMICON),
+			LR_DEFAULTCOLOR);
+
+	// set the window you want to recieve event messages
+	niData.hWnd = hWnd;
+
+	// set the message to send
+	// note: the message value should be in the
+	// range of WM_APP through 0xBFFF
+	niData.uCallbackMessage = IDM_ICON_TRAY_MESSAGE;
+
+	lstrcpy(niData.szTip, L"Rfid Locker");
+
+	// NIM_ADD adds a new tray icon
+	Shell_NotifyIcon(NIM_ADD, &niData);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   //ShowWindow(hWnd, nCmdShow);
+   ShowWindow(hWnd, SW_HIDE);
+   UpdateWindow(hWnd);
+   AddTrayIcon(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+	case IDM_ICON_TRAY_MESSAGE:
+		switch (lParam)
+		{
+		case WM_LBUTTONDBLCLK:
+			ShowWindow(hWnd, SW_RESTORE);
+			break;
+		case WM_RBUTTONDOWN:
+		case WM_CONTEXTMENU:
+			ShowContextMenu(hWnd, lParam, wParam);
+		}
+		break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+		niData.uFlags = 0;
+		Shell_NotifyIcon(NIM_DELETE, &niData);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+void ShowContextMenu(HWND hWnd, DWORD lParam, DWORD wParam)
+{
+	POINT cursor;
+	GetCursorPos(&cursor);
+	HMENU hMenu = LoadMenu(NULL, MAKEINTRESOURCE(IDR_POPUP_MENU));
+	hMenu = GetSubMenu(hMenu, 0);
+	TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, cursor.x, cursor.y, 0, hWnd, NULL);
+}
+
+#ifndef __WIN32OS2__
+// Get dll version number
+ULONGLONG GetDllVersion(LPCTSTR lpszDllName)
+{
+	ULONGLONG ullVersion = 0;
+	HINSTANCE hinstDll;
+	hinstDll = LoadLibrary(lpszDllName);
+	if (hinstDll)
+	{
+		DLLGETVERSIONPROC pDllGetVersion;
+		pDllGetVersion = (DLLGETVERSIONPROC)GetProcAddress(hinstDll, "DllGetVersion");
+		if (pDllGetVersion)
+		{
+			DLLVERSIONINFO dvi;
+			HRESULT hr;
+			ZeroMemory(&dvi, sizeof(dvi));
+			dvi.cbSize = sizeof(dvi);
+			hr = (*pDllGetVersion)(&dvi);
+			if (SUCCEEDED(hr))
+				ullVersion = MAKEDLLVERULL(dvi.dwMajorVersion, dvi.dwMinorVersion, 0, 0);
+		}
+		FreeLibrary(hinstDll);
+	}
+	return ullVersion;
+}
+#endif
